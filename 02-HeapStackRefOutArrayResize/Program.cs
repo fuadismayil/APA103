59e80631f3b3ed6faba4bbc97662d4a2833cb7cb
fuadismayil <fuadzi-apa103@code.edu.az>
2026-03-14 21:03:55 +0400

    Created CustomArrResize method with ref and params keywords

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,19 @@
+﻿static void CustomArrResize(ref int[] nums, params int[] addedNums)
+{
+    int[] backupArray = nums;
+    nums = new int[nums.Length + addedNums.Length];
+    for (int i = 0; i < backupArray.Length; i++)
+    {
+        nums[i] = backupArray[i];
+    }
+    for (int i = 0; i < addedNums.Length; i++)
+    {
+        nums[backupArray.Length + i] = addedNums[i];
+    }
+}
+int[] prices = [100, 200, 300, 400, 500];
+CustomArrResize(ref prices,600,700,800);
+for(int i = 0; i < prices.Length; i++)
+{
+    Console.WriteLine(prices[i]);
+}
