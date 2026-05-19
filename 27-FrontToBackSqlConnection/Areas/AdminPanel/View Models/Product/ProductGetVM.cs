2b31b0017d147d2dcb9395e7ba1221b7db0c5989
fuadismayil <fuadzi-apa103@code.edu.az>
2026-05-19 04:27:20 +0400

    task33

---
+++ 27-FrontToBackSqlConnection/Areas/AdminPanel/View Models/Product/ProductGetVM.cs
@@ -0,0 +1,14 @@
+﻿using _27_FrontToBackSqlConnection.Models;
+
+namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.View_Models.Product
+{
+    public class ProductGetVM
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string SKU { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
